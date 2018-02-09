fb9fb9ab2f8dd1622d3dccca6dc7be017da38ba3
Marcus Aaron M. Baga <marcusaaron.baga@sparkglobaltech.com>
2018-02-09 14:02:56 +0800

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC81zngVVQ3e71n-BY4vPwgn0np31aIGWQ",
+    authDomain: "fs1prod-efb86.firebaseapp.com",
+    databaseURL: "https://fs1prod-efb86.firebaseio.com",
+    projectId: "fs1prod-efb86",
+    storageBucket: "fs1prod-efb86.appspot.com",
+    messagingSenderId: "693191018816"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC81zngVVQ3e71n-BY4vPwgn0np31aIGWQ",
+    authDomain: "fs1prod-efb86.firebaseapp.com",
+    databaseURL: "https://fs1prod-efb86.firebaseio.com",
+    projectId: "fs1prod-efb86",
+    storageBucket: "fs1prod-efb86.appspot.com",
+    messagingSenderId: "693191018816"
+  }
 };
